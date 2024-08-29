e67b9a1c9767717cd47dd28c13d3ec3b0d993730
Rohan Rajendra Nikam <152033186+rohan848382@users.noreply.github.com>
2024-08-29 11:55:17 +0530

    Create variable.tf

---
+++ Day-7/variable.tf
@@ -0,0 +1,18 @@
+variable "ami" {
+    description = "inserting value the  for the ami"
+    type = string
+    default = ""
+  
+}
+
+variable "instance_type" {
+  description = "inserting value the for the instance_type"
+  type = string
+  default = ""
+}
+
+variable "key_name" {
+  description = "inserting value the for key_name"
+  type = string
+  default = ""
+}
